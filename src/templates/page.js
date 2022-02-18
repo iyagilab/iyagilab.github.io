c9ce9b598fa4bafb5793b5fb2766cec34364cf99
woo5gi <jeonuka14@naver.com>
2022-02-18 13:15:37 +0900

    test

--- src/templates/page.js
+++ src/templates/page.js
@@ -33,14 +33,14 @@ export default function PageTemplate({ data }) {
       <Helmet>
         <title>{title}</title>
 
-        <meta name="description" content={body} />
+        {/* <meta name="description" content={body} /> */}
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
 
         <meta property="og:type" content="website" />
         <meta property="og:title" content={title} />
-        <meta property="og:description" content={body} />
-        <meta property="og:image" content={featuredImage} />
+        {/* <meta property="og:description" content={body} /> */}
+        {/* <meta property="og:image" content={featuredImage} /> */}
         {/* <meta property="og:url" content={url} /> */}
         <meta property="og:site_name" content={title} />
       </Helmet>
